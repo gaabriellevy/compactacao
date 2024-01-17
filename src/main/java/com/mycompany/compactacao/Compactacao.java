4aef5183351ea34f13dce3b25858bd808bcfdf58
Gabriel Levy <levygabriel11@gmail.com>
2024-01-17 10:12:04 -0300

    Algoritmo de frequência

---
+++ src/main/java/com/mycompany/compactacao/Compactacao.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.compactacao;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author aluno
+ */
+public class Compactacao {
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Escreva o arquivo de entrada: ");
+        Scanner in = new Scanner(System.in);
+        String caminhoArquivoEntrada = in.nextLine();
+
+        File arquivoEntrada = new File(caminhoArquivoEntrada);
+        Scanner leitorEntrada = new Scanner(arquivoEntrada);
+        File arquivoSaida = new File("compactado.txt");
+        FileWriter writer = new FileWriter(arquivoSaida);
+
+        Compactador compactador = new Compactador();
+
+        while(leitorEntrada.hasNextLine()) { // lê as linha do arquivo
+            String linha = leitorEntrada.nextLine();
+            String linhaCompactada = compactador.frequenciaCaracteres(linha);
+            writer.write(linhaCompactada+"\n");
+        }
+        
+        writer.close();
+    }
+}
---
+++ src/main/java/com/mycompany/compactacao/Compactador.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.compactacao;
+
+/**
+ *
+ * @author aluno
+ */
+public class Compactador {
+    
+    public String frequenciaCaracteres(String entrada) {
+        StringBuilder comprimido = new StringBuilder();
+        
+        int ocorrencias = 1;
+        for (int i = 0; i < entrada.length(); i++) {
+            if (i < entrada.length() - 1 && entrada.charAt(i) == entrada.charAt(i + 1)) {
+                ocorrencias++;
+            } else {
+                comprimido.append(ocorrencias);
+                comprimido.append(entrada.charAt(i));
+                ocorrencias = 1; // reseta a 
+            }
+        }
+        
+        return comprimido.toString();
+        
+    }
+    
+}
